f71acb17aed429a627f9c6aec41d45cfedc7eab2
04ISTSA <04ISTSA@LIDT-S4X33633>
2021-11-22 15:25:52 +0100

    Add project files.

---
+++ Prodject uppgift 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace Prodject_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Annvändaren ska kunna ändra låtar i topp listan
+
+            // Annvändaren ska kunna söka efter låtar i topplistan
+            
+
+
+            Console.WriteLine("1. Ändra eller lägg till låtar i topplistan");
+            int menyVal = 1;
+            
+            string val = Console.ReadLine();
+            string filnamn = Console.ReadLine();
+            
+            switch (menyVal)
+            {
+                case 1:
+                    Console.WriteLine("Skriv in en låt som ska skrivas in/över");
+                    string rad = Console.ReadLine();
+                    File.WriteAllText(filnamn, rad);
+                    Console.WriteLine("Låten är sparad i filen");
+                    break;
+            }
+
+           
+            
+        }
+    }
+}
