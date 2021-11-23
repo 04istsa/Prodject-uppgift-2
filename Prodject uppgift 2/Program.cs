0cd4577106d90c3ae1ac930665cfc4f106380a26
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-11-23 09:22:39 +0100

    färdig med 1

--- Prodject uppgift 2/Program.cs
+++ Prodject uppgift 2/Program.cs
@@ -15,24 +15,51 @@ namespace Prodject_uppgift_2
             //HTest dfoi
 
             Console.WriteLine("1. Ändra eller lägg till låtar i topplistan");
-            int menyVal = 1;
-            string[] songs = new string[20];
-           // string val = Console.ReadLine();
-           // string filnamn = Console.ReadLine();
+
+            string filnamn = @"c:\Temp\Annteckningar.txt";
             
+            int menyVal = int.Parse(Console.ReadLine());
+            string[] songs = new string[5];
+            // string val = Console.ReadLine();
+            // string filnamn = Console.ReadLine();
+            
+
             switch (menyVal)
             {
                 case 1:
-                    for (int i = 0; i < 20; i++) {
+                    Console.WriteLine("Skriv in fem låtar");
+                    for (int i = 0; i < 5; i++) {
                         songs[i] = Console.ReadLine();
+                    
+                    
                     }
+                    File.WriteAllLines(filnamn, songs);
                     break;
                 case 2:
                     Console.WriteLine("Skriv in en låt som ska skrivas in/över");
                     string rad = Console.ReadLine();
-                   // File.WriteAllText(filnamn, rad);
+                   //File.WriteAllText(filnamn, rad);
                     Console.WriteLine("Låten är sparad i filen");
+
+
                     break;
+                case 3:
+                    Console.WriteLine("Sök efter en låt");
+                    string sök = Console.ReadLine();
+                        
+                    for (int i = 0; i < songs.Length; i++) {
+                        if (songs[i].Contains(sök))
+                        {
+                            Console.WriteLine($"{songs[i]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Låten du söker finns inte");
+                        }
+                        
+                    }
+                    break;
+                    
                     
             }
 
