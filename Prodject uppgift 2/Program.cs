ac3f7c54a2ff60f51de0161ff8afb01f2bce522e
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-11-23 08:01:46 +0100

    test

--- Prodject uppgift 2/Program.cs
+++ Prodject uppgift 2/Program.cs
@@ -12,22 +12,28 @@ namespace Prodject_uppgift_2
 
             // Annvändaren ska kunna söka efter låtar i topplistan
             
-
+            //HTest dfoi
 
             Console.WriteLine("1. Ändra eller lägg till låtar i topplistan");
             int menyVal = 1;
-            
-            string val = Console.ReadLine();
-            string filnamn = Console.ReadLine();
+            string[] songs = new string[20];
+           // string val = Console.ReadLine();
+           // string filnamn = Console.ReadLine();
             
             switch (menyVal)
             {
                 case 1:
+                    for (int i = 0; i < 20; i++) {
+                        songs[i] = Console.ReadLine();
+                    }
+                    break;
+                case 2:
                     Console.WriteLine("Skriv in en låt som ska skrivas in/över");
                     string rad = Console.ReadLine();
-                    File.WriteAllText(filnamn, rad);
+                   // File.WriteAllText(filnamn, rad);
                     Console.WriteLine("Låten är sparad i filen");
                     break;
+                    
             }
 
            
