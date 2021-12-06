6ee1fdbff892723e11801a17ffe4f705cbb62e16
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-12-06 15:49:15 +0100

    Färdig projekt uppgift 2

--- Prodject uppgift 2/Program.cs
+++ Prodject uppgift 2/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
+using System.Threading;
 using System.IO;
+using System.Text;
 namespace Prodject_uppgift_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
 
             // Annvändaren ska kunna ändra låtar i topp listan
 
             // Annvändaren ska kunna söka efter låtar i topplistan
             
             //HTest dfoi
-
-            Console.WriteLine("1. Ändra eller lägg till låtar i topplistan");
-            Console.WriteLine("3. Sök efter en låt");
-
-            string filnamn = @"c:\Temp\Annteckningar.txt";
             
-            int menyVal = int.Parse(Console.ReadLine());
-            string[] songs = new string[5];
+            
+
+
+
+            string filnamn = @"c:\Temp\Annteckningar2.txt";
+            
+            
+            string[] songs;
             // string val = Console.ReadLine();
             // string filnamn = Console.ReadLine();
             if (File.Exists(filnamn))
@@ -30,36 +33,55 @@ namespace Prodject_uppgift_2
             }
             else
             {
-                songs = new string[5];
+                songs = new string[20];
                 for (int i = 0; i < songs.Length; i++)
                 {
                     songs[i] = "tom";
                 }
                 File.WriteAllLines(filnamn, songs);
             }
-
-
+            int menyVal = 0;
+           
+            while (menyVal != 5) { 
+            Console.WriteLine("1. Visa din topplista");
+            Console.WriteLine("2. Skriva in låtar i topplistan");
+            Console.WriteLine("3. Ändra låtar i topplistan");
+            Console.WriteLine("4. Sök efter en låt");
+            Console.WriteLine("5. Avsluta programmet!\n");
+            
+           menyVal = int.Parse(Console.ReadLine());
 
             switch (menyVal)
             {
                 case 1:
+                    if (File.Exists(filnamn))
+                    {
+                        // Skriver ut texten från filen
+                        string filinnehåll = File.ReadAllText(filnamn);
+                        Console.WriteLine("Här är filens innehåll:");
+                        Console.WriteLine(filinnehåll);
+                    }
+                    break;
+
+                case 2:
                     Console.WriteLine("Skriv in fem låtar");
-                    for (int i = 0; i < 5; i++) {
+                    for (int i = 0; i < 20; i++) {
                         songs[i] = Console.ReadLine();
                     
                     
                     }
                     File.WriteAllLines(filnamn, songs);
                     break;
-                case 2:
-                    Console.WriteLine("Skriv in en låt som ska skrivas in/över");
-                    string rad = Console.ReadLine();
-                   //File.WriteAllText(filnamn, rad);
-                    Console.WriteLine("Låten är sparad i filen");
 
-
-                    break;
                 case 3:
+                    Console.WriteLine("Vilken låt vill du ändra");
+                    int låt = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv in en ny låt i topplistan");
+                    songs[låt - 1] = Console.ReadLine();
+                    File.WriteAllLines(filnamn, songs);
+                    break;
+               
+                case 4:
                     Console.WriteLine("Sök efter en låt");
                     string sök = Console.ReadLine().ToLower();
                    
@@ -67,7 +89,7 @@ namespace Prodject_uppgift_2
                     for (int i = 0; i < songs.Length; i++) {
                         if (songs[i].ToLower().Contains(sök))
                         {
-                            Console.WriteLine($"Låten {songs[i]} finns i spel listan");
+                            Console.WriteLine($"Låten {songs[i]} finns i spel listan på index {i}");
                             finns++;
                         }
                        
@@ -82,11 +104,21 @@ namespace Prodject_uppgift_2
                     }
 
                     break;
-                    
-                    
+                case 5:
+                    // Avslutar programmet
+                    Console.WriteLine("Programmet avslutas");
+                    Thread.Sleep(1000);
+                    break;
+
+                default:
+                    // Om man inte skrev ett gilltigt alternativ
+                    Console.WriteLine("Du skrev inte ett gilltigt alternativ, försök igen!");
+                    break;
+
+
             }
 
-           
+           }
             
         }
     }
